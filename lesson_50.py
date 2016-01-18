55f68830e41676d52b530194b9bb1474ee114e38
SigmaQuan <quan.zhibin@gmail.com>
2016-01-18 17:18:44 +0800

    Update lesson 50

---
+++ lesson_50.py
@@ -0,0 +1,29 @@
+"""
+Exercise 50: Your First Website
+
+These final three exercises will be very hard and you should take your
+time with them. In this first one you'll build a simple web version of
+one of your games. Before you attempt this exercise you must have
+completed Exercise 46 successfully and have a working pip installed
+such that you can install packages and know how to make a skeleton
+project directory. If you don't remember how to do this, go back to
+Exercise 46 and do it all over again.
+Installing lpthw.web
+
+Before creating your first web application, you'll first need to
+install the "web framework" called lpthw.web. The term "framework"
+generally means "some package that makes it easier for me to do
+something." In the world of web applications, people create "web
+frameworks" to compensate for the difficult problems they've
+encountered when making their own sites. They share these common
+solutions in the form of a package you can download to bootstrap your
+own projects.
+
+In our case, we'll be using the lpthw.web framework, but there are
+many, many, many others you can choose from. For now, learn lpthw.web,
+then branch out to another one when you're ready (or just keep using
+lpthw.web since it's good enough).
+
+Using pip, install lpthw.web:
+
+"""
