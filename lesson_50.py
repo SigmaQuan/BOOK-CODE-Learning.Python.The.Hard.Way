70b61f6915542267388dd5ed1ab85e41a7896815
SigmaQuan <quan.zhibin@gmail.com>
2016-01-21 22:35:44 +0800

    Finish updating lesson 50

--- lesson_50.py
+++ lesson_50.py
@@ -251,13 +251,14 @@ function you call on render is just matched to an .html file in
 templates/ .
 
 
-
-
-
-
-
-
-
-
-
+Study Drills
+    1. Read the document at http://webpy.org/ which is the same as the
+    lpthw.web project.
+    2. Experiment with everything you can find there, including their
+    example code.
+    3. Read about HTML5 and CSS3 and make some other .html and .css
+    files for practice.
+    4. If you have a friend who knows Django and is willing to help
+    you, then consider doing Exercises 50, 51, and 52 in Django
+    instead to see what that's like.
 """
